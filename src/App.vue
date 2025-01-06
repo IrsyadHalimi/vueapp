98ddb7f019516623ce12f5c76418121b21ac0cdd
IrsyadHalimi <irsyadhalimi08@gmail.com>
2025-01-06 15:44:42 +0700

    Showing text

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
+<script setup>
+import MyCounter from './components/MyCounter.vue'
+</script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyCounter/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+<template>
+  <div>
+    <p class="red-text">My Counter Component</p>
+  </div>
+</template>
+<style scoped>
+.red-text {
+  color: red;
+}
+</style>
