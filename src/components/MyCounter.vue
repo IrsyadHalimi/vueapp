886789a8dd510276cc9491db20bfa22fba98c0a2
IrsyadHalimi <irsyadhalimi08@gmail.com>
2025-01-07 09:43:41 +0700

    dateFormat

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,26 +1,53 @@
 <script setup>
-import useMaximum from "../composables/useMaximum.js";
+import { customRef } from "vue";
 
-const maximum = 10;
-
-const count = useMaximum(maximum);
-
-function increment() {
-  count.value += 1;
+const formatDate = (date, format) => {
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  if (format == "MM-DD-YYYY")
+    return date.toLocaleDateString('en-US', options).replace(/\//g, '-');
+  else if (format == "DD-MM-YYYY")
+    return date.toLocaleDateString('en-GB', options).replace(/\//g, '-');
+  else if (format == "MM/DD/YYYY")
+    return date.toLocaleDateString('en-US', options);
+  else if (format == "DD/MM/YYYY")
+    return date.toLocaleDateString('en-GB', options);
 }
 
-function decrement() {
-  count.value -= 1;
-}
+const useFormatDate = (date, format) => {
+  return customRef((track, trigger) => {
+    let value = date;
+    return {
+      get() {
+        track();
+        return formatDate(value, format);
+      },
+      set(newValue) {
+        value = newValue;
+        trigger();
+      }
+    };
+  });
+};
+
+const date = new Date();
+
+const dateMMDDYYYY = useFormatDate(date, "MM-DD-YYYY");
+const dateDDMMYYYY = useFormatDate(date, "DD-MM-YYYY");
+const dateMMDDYYYY_slash = useFormatDate(date, "MM/DD/YYYY");
+const dateDDMMYYYY_slash = useFormatDate(date, "DD/MM/YYYY");
 
 </script>
 <template>
   <h3>MyCounter Component</h3>
-  Reactive variable count: <b>{{ count }}</b>
+  <hr>
+  Current date: {{ date }}
+  <hr>
+  Date (MM-DD-YYYY) : <b>{{ dateMMDDYYYY }}</b>
   <br><br>
-  Maximum: <b>{{ maximum }}</b>
+  Date (DD-MM-YYYY) : <b>{{ dateDDMMYYYY }}</b>
   <br><br>
-  <button @click="increment">count + 1</button>
-  &nbsp;
-  <button @click="decrement">count - 1</button>
+  Date (MM/DD/YYYY) : <b>{{ dateMMDDYYYY_slash }}</b>
+  <br><br>
+  Date (DD/MM/YYYY) : <b>{{ dateDDMMYYYY_slash }}</b>
+  <br><br>
 </template>
