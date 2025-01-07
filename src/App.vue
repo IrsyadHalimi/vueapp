8cbac72aed6e7c1bd9cd9e8550f64a7c867b0312
IrsyadHalimi <irsyadhalimi08@gmail.com>
2025-01-07 08:32:44 +0700

    show vue lifecycle method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MyCounter from './components/MyCounter.vue'
 </script>
+
 <template>
   <MyCounter/>
 </template>
--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,12 +1,48 @@
 <script setup>
+import { ref, computed, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from "vue";
+
+console.log("setup: The component is created in memory");
+
+const count = ref(0);
+
+const doubleCount = computed(function() {
+  return count.value * 2;
+});
+
+const increment = () => {
+  count.value++;
+};
+
+onBeforeMount(() => {
+  console.log("onBeforeMount: The component is about to be mounted in the DOM.");
+});
+
+onMounted(() => {
+  console.log("onMounted: The component has been mounted in the DOM");
+});
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate: The component is about to be updated.");
+});
+
+onUpdated(() => {
+  console.log("onUpdated: The component has been updated.");
+});
+
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount: The component is about to be unmounted.");
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted: The component has been unmounted.");
+})
+
 </script>
 <template>
-  <div>
-    <p class="red-text">My Counter Component</p>
-  </div>
+  <h3>MyCounter Component</h3>
+  Reactive variable count: <b>{{ count }}</b>
+  <br />
+  Computed variable doubleCount : <b>{{ doubleCount }}</b>
+  <br /><br />
+  <button @click="increment()">Count+1</button>
 </template>
-<style scoped>
-.red-text {
-  color: red;
-}
-</style>
