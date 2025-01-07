72bd85949f1894b9592685d69b72a0dae1f6ddb6
IrsyadHalimi <irsyadhalimi08@gmail.com>
2025-01-07 08:40:49 +0700

    automatic start counting

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,41 +1,33 @@
 <script setup>
-import { ref, computed, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from "vue";
+import { ref, computed, onMounted, onUnmounted } from "vue";
 
-console.log("setup: The component is created in memory");
+let timer;
 
 const count = ref(0);
 
-const doubleCount = computed(function() {
-  return count.value * 2;
-});
+const doubleCount = computed(() => count.value * 2);
 
 const increment = () => {
   count.value++;
 };
 
-onBeforeMount(() => {
-  console.log("onBeforeMount: The component is about to be mounted in the DOM.");
-});
+const start = () => {
+  timer = setInterval(function() {
+    increment();
+  }, 1000);
+};
+
+const stop = () => {
+  clearInterval(timer);
+};
 
 onMounted(() => {
-  console.log("onMounted: The component has been mounted in the DOM");
-});
-
-onBeforeUpdate(() => {
-  console.log("onBeforeUpdate: The component is about to be updated.");
-});
-
-onUpdated(() => {
-  console.log("onUpdated: The component has been updated.");
-});
-
-onBeforeUnmount(() => {
-  console.log("onBeforeUnmount: The component is about to be unmounted.");
+  start();
 });
 
 onUnmounted(() => {
-  console.log("onUnmounted: The component has been unmounted.");
-})
+  stop();
+});
 
 </script>
 <template>
@@ -43,6 +35,4 @@ onUnmounted(() => {
   Reactive variable count: <b>{{ count }}</b>
   <br />
   Computed variable doubleCount : <b>{{ doubleCount }}</b>
-  <br /><br />
-  <button @click="increment()">Count+1</button>
 </template>
