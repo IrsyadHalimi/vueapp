359aa32e4c94c897c76de789173068a57c45eda7
IrsyadHalimi <irsyadhalimi08@gmail.com>
2025-01-07 09:03:20 +0700

    use customRef

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,38 +1,32 @@
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from "vue";
+import { customRef } from "vue";
 
-let timer;
-
-const count = ref(0);
-
-const doubleCount = computed(() => count.value * 2);
-
-const increment = () => {
-  count.value++;
-};
-
-const start = () => {
-  timer = setInterval(function() {
-    increment();
-  }, 1000);
-};
-
-const stop = () => {
-  clearInterval(timer);
-};
-
-onMounted(() => {
-  start();
+const count = customRef((track, trigger) => {
+  let value = 0;
+  return {
+    get() {
+      track();
+      console.log("get value =", value);
+      return value;
+    },
+    set(newValue) {
+      value = newValue;
+      trigger();
+      console.log("set value =", value);
+    }
+  };
 });
 
-onUnmounted(() => {
-  stop();
-});
+function increment() {
+  console.log("before increment value");
+  count.value += 1;
+  console.log("after increment value");
+}
 
 </script>
 <template>
   <h3>MyCounter Component</h3>
   Reactive variable count: <b>{{ count }}</b>
   <br />
-  Computed variable doubleCount : <b>{{ doubleCount }}</b>
+  <button @click="increment">Count + 1</button>
 </template>
